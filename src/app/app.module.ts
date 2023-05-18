2ed9c75042ca1a0174b47dc5e34e41a426e5b588
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-05-18 13:47:11 +0700

    Ended Deploying an application_05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ShippingComponent } from './shipping/shipping.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+
 
 @NgModule({
   imports: [
@@ -32,6 +34,8 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductDetailsComponent,
     CartComponent,
     ShippingComponent,
+    HeroListComponent,
+
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/hero-list/hero-list.component.ts
@@ -0,0 +1,18 @@
+import { Component ,OnInit} from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-hero-list',
+  templateUrl: './hero-list.component.html',
+  styleUrls: ['./hero-list.component.css'],
+})
+export class HeroListComponent implements OnInit{
+
+
+  ngOnInit(){
+
+  }
+
+
+}
